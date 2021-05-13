5cf72ac8e6b8521575c01d0687ac3c850339bdb7
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-05-13 20:31:14 +0800

    object literal

---
+++ src/lessons/lesson1/nn.js
@@ -0,0 +1,22 @@
+function DNAStrand(dna) {
+    const arr = dna.split("");
+    const letters = {
+        A: "T",
+        T: "A",
+        G: "C",
+        C: "G",
+    };
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = letters[arr[i]] || arr[i];
+    }
+    return arr.join("");
+}
+
+if (false) console.log("true");
+else console.log("false");
+
+console.log(DNAStrand("ATTGC")); // return "TAACG"
+
+console.log(DNAStrand("GTAT")); // return "CATA"
+
+console.log(DNAStrand("lala"));
